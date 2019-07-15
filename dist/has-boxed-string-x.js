a62c1883da3c8275696ebcb36cbacb0ebfae4932
Graham Fairweather <xotic750@gmail.com>
2019-07-15 12:22:35 +0200

    :bookmark: v2.0.4
    Auto build

--- dist/has-boxed-string-x.js
+++ dist/has-boxed-string-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-14T21:29:12.907Z",
+  "date": "2019-07-15T10:22:30.796Z",
   "describe": "",
   "description": "Check support of by-index access of string characters.",
   "file": "has-boxed-string-x.js",
   "hash": "76081e6792d466ff84c0",
   "license": "MIT",
-  "version": "2.0.3"
+  "version": "2.0.4"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
