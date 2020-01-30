cfdac6d35f5b01bcb5ee052e352ac290f2f23c3a
Graham Fairweather <xotic750@gmail.com>
2020-01-30 12:10:52 +0100

    Maintenance

--- dist/has-boxed-string-x.js
+++ dist/has-boxed-string-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-09-05T18:23:05.107Z",
+  "date": "2020-01-30T11:10:46.366Z",
   "describe": "",
   "description": "Check support of by-index access of string characters.",
   "file": "has-boxed-string-x.js",
